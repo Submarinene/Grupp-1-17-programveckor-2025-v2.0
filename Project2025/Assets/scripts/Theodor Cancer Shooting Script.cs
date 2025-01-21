d2a6b0554feffb022776c0d218e4359051c716f1
TheodorAndreasson <theodor.andreasson@elev.ga.lbs.se>
2025-01-21 13:37:25 +0100

    fixar latcho laiban cancer funkar

---
+++ Project2025/Assets/scripts/Theodor Cancer Shooting Script.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TheodorCancerShootingScript : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletPos;
+    bool isPlayerInRange = false;
+    float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (isPlayerInRange)
+        {
+            if (timer > 2)
+            {
+                Shoot();
+                timer = 0;
+
+            }
+        }
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("MainBody"))
+        {
+            isPlayerInRange = false;
+        }
+    }
+    void Shoot()
+    {
+        Instantiate(bullet, bulletPos.position, Quaternion.identity);
+    }
+}
